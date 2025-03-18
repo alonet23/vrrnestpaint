83251d1ac5d82806048f2d9e8245ed6f65fc7517
alonet23 <82866219+alonet23@users.noreply.github.com>
2025-03-18 10:12:03 +0530

    Update page.tsx
    check in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'; 
 import React, { useState, useEffect } from 'react';
 import { Calendar, Clock, CheckSquare, AlertTriangle, Home, Bell, Users, PaintBucket, BarChart, FileText, LogOut } from 'lucide-react';
 
